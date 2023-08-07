d47afe44ef7433b41682ddd09170c40268401d59
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-08-07 18:06:59 -0300

    Included OpenNLP and DL4J

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import opennlp.tools.tokenize.SimpleTokenizer;
+import org.nd4j.linalg.factory.Nd4j;
+
 public class Main {
     public static void main(String[] args) {
+        Nd4j.zeros(3,5);
         System.out.println("Hello world!");
     }
 }
