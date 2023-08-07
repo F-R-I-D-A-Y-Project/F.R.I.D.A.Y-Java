3b362706b2ae1481f660d5d1324616254570b824
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-08-07 18:40:57 -0300

    Debugged NL4J

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
 import opennlp.tools.tokenize.SimpleTokenizer;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
 public class Main {
     public static void main(String[] args) {
-        Nd4j.zeros(3,5);
-        System.out.println("Hello world!");
+        INDArray Something = Nd4j.zeros(3,5);
+        System.out.println(Something);
     }
 }
