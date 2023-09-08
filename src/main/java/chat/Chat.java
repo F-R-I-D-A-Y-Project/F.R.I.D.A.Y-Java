bbfcb4b6cf599254f8f3e53cbe03bafaec49fe9d
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-09-08 17:35:25 -0300

    Chatbot application nearly done

--- src/main/java/chat/Chat.java
+++ src/main/java/chat/Chat.java
@@ -1,42 +1,80 @@
 package chat;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import model.Model;
+//import model.Model;
 
 public class Chat extends JFrame{
-    private JTextArea chatArea = new JTextArea();
-    private JTextField chatBox = new JTextField();
-    private Model model = new Model();
+    private final JTextArea chatArea = new JTextArea();
+    private final JTextField chatBox = new JTextField();
+
+    //    private final Model model = new Model();
     public Chat() {
         JFrame frame = new JFrame();
         frame.setTitle("F.R.I.D.A.Y");
         frame.setResizable(false);
         frame.setLayout(null);
-        frame.setSize(500, 500);
+        frame.setSize(620, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.add(chatArea);
         frame.add(chatBox);
 
-        chatArea.setSize(500, 400);
+        chatArea.setSize(620, 700);
         chatArea.setLocation(2, 2);
+        chatArea.append("F.R.I.D.A.Y: Hello, I am F.R.I.D.A.Y, your personal assistant. How may I help you?\n\n");
+        Font font = new Font("Arial", Font.BOLD, 16);
+        chatArea.setFont(font);
+        chatArea.setLineWrap(true);
+        chatArea.setWrapStyleWord(true);
+        chatArea.setEditable(false);
 
         chatBox.setSize(500, 30);
-        chatBox.setLocation(2, 420);
-
+        chatBox.setLocation(0, 702);
+        int thickness = 2;
+        Border customBorder = BorderFactory.createMatteBorder(thickness, thickness, thickness, thickness, Color.BLACK);
+        chatBox.setBorder(customBorder);
         chatBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String gtext = chatBox.getText();
-                chatArea.append("YOU: " + gtext + "\n");
-                chatArea.append("F.R.I.D.A.Y: " + botResponse(gtext) + "\n");
+                String text = chatBox.getText();
+                chatArea.append("YOU: " + text + "\n\n");
+                chatArea.append("F.R.I.D.A.Y: " + botResponse(text) + "\n\n");
                 chatBox.setText("");
             }
         });
 
+        JButton send = sendButton();
+        send.setLocation(500, 700);
+        frame.add(send);
+
+        int scrollBarThickness = 20;
+        JScrollPane scroll = new JScrollPane(chatArea);
+        scroll.setSize(620, 700);
+        scroll.setLocation(2,2);
+        frame.add(scroll);
+
     }
+
+    private JButton sendButton() {
+        JButton send = new JButton("Send");
+        send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = chatBox.getText();
+                chatArea.append("YOU: " + text + "\n\n");
+                chatArea.append("F.R.I.D.A.Y: " + botResponse(text) + "\n\n");
+                chatBox.setText("");
+            }
+        });
+
+        send.setSize(120, 32);
+        return send;
+    }
+
     private String botResponse(String input){
 //        model
         return "Huh?";
